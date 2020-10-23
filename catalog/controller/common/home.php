93e2d8e682f11744cf5c2fe82c60a44884d380f8
Vladij <vladij01@gmail.com>
2020-10-23 08:42:35 +0300

    fix bugs. Images, products from main

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -8,6 +8,245 @@ class ControllerCommonHome extends Controller {
 		if (isset($this->request->get['route'])) {
 			$this->document->addLink($this->config->get('config_url'), 'canonical');
 		}
+        $this->load->language('product/category');
+
+        $this->load->model('catalog/category');
+
+        $this->load->model('catalog/product');
+
+        $this->load->model('tool/image');
+
+        if (isset($this->request->get['filter'])) {
+            $filter = $this->request->get['filter'];
+        } else {
+            $filter = '';
+        }
+
+        if (isset($this->request->get['sort'])) {
+            $sort = $this->request->get['sort'];
+        } else {
+            $sort = 'p.sort_order';
+        }
+
+        if (isset($this->request->get['order'])) {
+            $order = $this->request->get['order'];
+        } else {
+            $order = 'ASC';
+        }
+
+        if (isset($this->request->get['page'])) {
+            $page = $this->request->get['page'];
+        } else {
+            $page = 1;
+        }
+
+        if (isset($this->request->get['limit'])) {
+            $limit = (int)$this->request->get['limit'];
+        } else {
+            $limit = $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit');
+        }
+
+
+        $data['compare'] = $this->url->link('product/compare');
+
+
+        function str_replace_once($search, $replace, $text)
+        {
+            $pos = strpos($text, $search);
+            return $pos!==false ? substr_replace($text, $replace, $pos, strlen($search)) : $text;
+        }
+
+
+        $data['products'] = array();
+
+        $filter_data = array(
+            'filter_category_id' => 0,
+            'filter_filter'      => $filter,
+            'sort'               => $sort,
+            'order'              => $order,
+            'start'              => ($page - 1) * $limit,
+            'limit'              => $limit
+        );
+
+        $product_total = $this->model_catalog_product->getTotalProducts($filter_data);
+
+        $results = $this->model_catalog_product->getProducts($filter_data);
+
+        foreach ($results as $result) {
+            if ($result['image']) {
+                $image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+            } else {
+                $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+            }
+
+            if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+                $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+            } else {
+                $price = false;
+            }
+
+            if ((float)$result['special']) {
+                $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+            } else {
+                $special = false;
+            }
+
+            if ($this->config->get('config_tax')) {
+                $tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+            } else {
+                $tax = false;
+            }
+
+            if ($this->config->get('config_review_status')) {
+                $rating = (int)$result['rating'];
+            } else {
+                $rating = false;
+            }
+
+            $data['products'][] = array(
+                'product_id'  => $result['product_id'],
+                'thumb'       => $image,
+                'name'        => $result['name'],
+                'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+                'price'       => $price,
+                'special'     => $special,
+                'tax'         => $tax,
+                'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
+                'rating'      => $result['rating'],
+                'href'        =>   $this->url->link('product/product', 'path=' . null . '&product_id=' . $result['product_id'] )
+            );
+        }
+
+
+
+        $url = '';
+
+        if (isset($this->request->get['filter'])) {
+            $url .= '&filter=' . $this->request->get['filter'];
+        }
+
+        if (isset($this->request->get['limit'])) {
+            $url .= '&limit=' . $this->request->get['limit'];
+        }
+
+
+        $data['sorts'] = array();
+
+        $data['sorts'][] = array(
+            'text'  => $this->language->get('text_default'),
+            'value' => 'p.sort_order-ASC',
+        );
+
+        $data['sorts'][] = array(
+            'text'  => $this->language->get('text_name_asc'),
+            'value' => 'pd.name-ASC',
+        );
+
+        $data['sorts'][] = array(
+            'text'  => $this->language->get('text_name_desc'),
+            'value' => 'pd.name-DESC',
+        );
+
+        $data['sorts'][] = array(
+            'text'  => $this->language->get('text_price_asc'),
+            'value' => 'p.price-ASC',
+        );
+
+        $data['sorts'][] = array(
+            'text'  => $this->language->get('text_price_desc'),
+            'value' => 'p.price-DESC',
+        );
+
+        if ($this->config->get('config_review_status')) {
+            $data['sorts'][] = array(
+                'text'  => $this->language->get('text_rating_desc'),
+                'value' => 'rating-DESC',
+            );
+
+            $data['sorts'][] = array(
+                'text'  => $this->language->get('text_rating_asc'),
+                'value' => 'rating-ASC',
+            );
+        }
+
+        $data['sorts'][] = array(
+            'text'  => $this->language->get('text_model_asc'),
+            'value' => 'p.model-ASC',
+        );
+
+        $data['sorts'][] = array(
+            'text'  => $this->language->get('text_model_desc'),
+            'value' => 'p.model-DESC',
+        );
+
+
+
+
+        $data['limits'] = array();
+
+        $limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 25, 50, 75, 100));
+
+        sort($limits);
+
+
+        $url = '';
+
+        if (isset($this->request->get['filter'])) {
+            $url .= '&filter=' . $this->request->get['filter'];
+        }
+
+        if (isset($this->request->get['sort'])) {
+            $url .= '&sort=' . $this->request->get['sort'];
+        }
+
+        if (isset($this->request->get['order'])) {
+            $url .= '&order=' . $this->request->get['order'];
+        }
+
+
+
+        foreach($limits as $value) {
+            $data['limits'][] = array(
+                'text'  => $value,
+                'value' => $value
+            );
+        }
+
+
+        $url = '';
+
+        if (isset($this->request->get['filter'])) {
+            $url .= '&filter=' . $this->request->get['filter'];
+        }
+
+        if (isset($this->request->get['sort'])) {
+            $url .= '&sort=' . $this->request->get['sort'];
+        }
+
+        if (isset($this->request->get['order'])) {
+            $url .= '&order=' . $this->request->get['order'];
+        }
+
+        if (isset($this->request->get['limit'])) {
+            $url .= '&limit=' . $this->request->get['limit'];
+        }
+
+        $url = str_replace_once('&', '?', $url);
+
+
+        $pagination = new Pagination();
+        $pagination->total = $product_total;
+        $pagination->page = $page;
+        $pagination->limit = $limit;
+
+        $data['pagination'] = $pagination->render();
+
+        $data['results'] = sprintf($this->language->get('text_pagination'), ($product_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($product_total - $limit)) ? $product_total : ((($page - 1) * $limit) + $limit), $product_total, ceil($product_total / $limit));
+
+
+        $data['sort'] = $sort;
+        $data['order'] = $order;
+        $data['limit'] = $limit;
 
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
